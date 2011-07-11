55634b414a3f04e9b4e0316f18ffacd59fab5b86
wsun <>
2011-07-11 19:38:02 +0000

    Update to new VectorInputSource with memory improvements.

--- interface/PileUp.h
+++ interface/PileUp.h
@@ -3,10 +3,18 @@
 
 #include <string>
 #include <vector>
+#include <boost/bind.hpp>
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Sources/interface/VectorInputSource.h"
 #include "DataFormats/Provenance/interface/EventID.h"
+#include "FWCore/Framework/interface/EventPrincipal.h"
 
+#include "CLHEP/Random/RandPoissonQ.h"
+#include "CLHEP/Random/RandFlat.h"
+
+#include "TRandom.h"
+#include "TFile.h"
+#include "TH1F.h"
 
 class TFile;
 class TH1F;
@@ -20,15 +28,18 @@ namespace CLHEP {
 namespace edm {
   class PileUp {
   public:
-    typedef VectorInputSource::EventPrincipalVector EventPrincipalVector;
-    explicit PileUp(ParameterSet const& pset, int const minb, int const maxb, double averageNumber, TH1F* const histo, const bool playback);
+    explicit PileUp(ParameterSet const& pset, double averageNumber, TH1F* const histo, const bool playback);
     ~PileUp();
 
-    void readPileUp(std::vector<EventPrincipalVector> & result,std::vector<std::vector<edm::EventID> > &ids);
+    template<typename T>
+      void readPileUp(std::vector<edm::EventID> &ids, T eventOperator);
+
+    template<typename T>
+      void playPileUp(const std::vector<edm::EventID> &ids, T eventOperator);
 
     double averageNumber() const {return averageNumber_;}
     bool poisson() const {return poisson_;}
-    bool doPileup() {return none_ ? false :  averageNumber_>0.;}
+    bool doPileUp() {return none_ ? false :  averageNumber_>0.;}
     void dropUnwantedBranches(std::vector<std::string> const& wantedBranches) {
       input_->dropUnwantedBranches(wantedBranches);
     }
@@ -36,10 +47,12 @@ namespace edm {
       input_->doEndJob();
     }
 
+    //template<typename T>
+    // void recordEventForPlayback(EventPrincipal const& eventPrincipal,
+	//			  std::vector<edm::EventID> &ids, T& eventOperator);
+
   private:
     std::string const type_;
-    int const minBunch_;
-    int const maxBunch_;
     double const averageNumber_;
     int const intAverage_;
     TH1F* const histo_;
@@ -64,6 +77,104 @@ namespace edm {
     // read the seed for the histo and probability function cases
     int seed_;
   };
+
+
+
+  template<typename T>
+  class RecordEventID
+  {
+  private:
+    std::vector<edm::EventID>& ids_;
+    T& eventOperator_;
+    int eventCount ;
+  public:
+    RecordEventID(std::vector<edm::EventID>& ids, T& eventOperator)
+      : ids_(ids), eventOperator_(eventOperator), eventCount( 0 ) {}
+    void operator()(EventPrincipal const& eventPrincipal) {
+      ids_.push_back(eventPrincipal.id());
+      eventOperator_(eventPrincipal, ++eventCount);
+    }
+  };
+
+
+  /*! Generates events from a VectorInputSource.
+   *  This function decides which method of VectorInputSource 
+   *  to call: sequential, random, or pre-specified.
+   *  The ids are either ids to read or ids to store while reading.
+   *  eventOperator has a type that matches the eventOperator in
+   *  VectorInputSource::loopRandom.
+   */
+  template<typename T>
+  void
+  PileUp::readPileUp(std::vector<edm::EventID> &ids, T eventOperator) {
+
+    // This is a number of events per source per bunch crossing.
+    size_t pileEventCnt=0;
+
+/*     if (sequential_) { */
+/*       // For now, the use case for sequential read reads only one */
+/*       // event at a time. */
+/*       pileEventCnt = 1; */
+/*     } else { */
+      
+      if (none_){
+        pileEventCnt = 0;
+      }else if (poisson_){
+        pileEventCnt = poissonDistribution_->fire();
+      }else if (fixed_){
+        pileEventCnt = intAverage_;
+      }else if (histoDistribution_ || probFunctionDistribution_){
+        double d = histo_->GetRandom();
+        pileEventCnt = int(d);
+      }
+      
+/*     } */
+
+    // One reason PileUp is responsible for recording event IDs is
+    // that it is the one that knows how many events will be read.
+    ids.reserve(pileEventCnt);
+    RecordEventID<T> recorder(ids,eventOperator);
+    if (sequential_) {
+      // boost::bind creates a functor from recordEventForPlayback
+      // so that recordEventForPlayback can insert itself before
+      // the original eventOperator.
+
+      input_->loopSequential(pileEventCnt, recorder);
+      //boost::bind(&PileUp::recordEventForPlayback<T>,
+      //                    boost::ref(*this), _1, boost::ref(ids),
+      //                             boost::ref(eventOperator))
+      //  );
+        
+    } else  {
+      input_->loopRandom(pileEventCnt, recorder);
+      //               boost::bind(&PileUp::recordEventForPlayback<T>,
+      //                             boost::ref(*this), _1, boost::ref(ids),
+      //                             boost::ref(eventOperator))
+      //                 );
+    }
+  }
+
+
+
+  template<typename T>
+  void
+  PileUp::playPileUp(const std::vector<edm::EventID> &ids, T eventOperator) {
+    input_->loopSpecified(ids,eventOperator);
+  }
+
+
+
+  /*! Record the event ID and pass the call on to the eventOperator.
+   */
+  /*  template<typename T>
+    void recordEventForPlayback(EventPrincipal const& eventPrincipal,
+                             std::vector<edm::EventID> &ids, T& eventOperator)
+    {
+      ids.push_back(eventPrincipal.id());
+      eventOperator(eventPrincipal);
+    }
+  */
 }
+
 
 #endif
--- src/PileUp.cc
+++ src/PileUp.cc
@@ -9,20 +9,11 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/RandomNumberGenerator.h"
 
-#include "CLHEP/Random/RandPoissonQ.h"
-#include "CLHEP/Random/RandFlat.h"
-
-#include "TRandom.h"
-#include "TFile.h"
-#include "TH1F.h"
-
 #include <algorithm>
 
 namespace edm {
-  PileUp::PileUp(ParameterSet const& pset, int const minb, int const maxb, double averageNumber, TH1F * const histo, const bool playback) :
+  PileUp::PileUp(ParameterSet const& pset, double averageNumber, TH1F * const histo, const bool playback) :
     type_(pset.getParameter<std::string>("type")),
-    minBunch_(minb),
-    maxBunch_(maxb),
     averageNumber_(averageNumber),
     intAverage_(static_cast<int>(averageNumber)),
     histo_(histo),
@@ -69,58 +60,5 @@ namespace edm {
     delete poissonDistribution_;
   }
 
-  void
-  PileUp::readPileUp(std::vector<EventPrincipalVector> & result,std::vector<std::vector<edm::EventID> > &ids) {
-            
-    for (int i = minBunch_; i <= maxBunch_; ++i) {
-      EventPrincipalVector eventVector;
-      int n=0;
-      
-      if (playback_){
-	n = ids[i-minBunch_].size();
-      } else if (sequential_) {
-	// For now, the use case for sequential read reads only one event at a time.
-	n = 1;
-      } else {
-	
-	if (none_){
-	  n = 0;
-	}else if (poisson_){
-	  n = poissonDistribution_->fire();
-	}else if (fixed_){
-	  n = intAverage_;
-	}else if (histoDistribution_ || probFunctionDistribution_){
-	  double d = histo_->GetRandom();
-	  //n = (int) floor(d + 0.5);  // incorrect for bins with integer edges
-	  n = int(d);
-	}
-
-      }
-      eventVector.reserve(n);
-      while (n > 0) {
-        EventPrincipalVector oneResult;
-        oneResult.reserve(n);
-	std::vector<edm::EventID> oneResultPlayback;
-	oneResultPlayback.reserve(n);
-	if (playback_)   {
-	  input_->readManySpecified(ids[i-minBunch_],oneResult);  // playback
-	} else if (sequential_) {
-	  unsigned int file;
-	  input_->readManySequential(n, oneResult, file);  // sequential
-	} else  {
-	  unsigned int file;   //FIXME: need unsigned filenr?
-	  input_->readManyRandom(n, oneResult,file);     //no playback
-	  for (int j=0;j<(int)oneResult.size();j++){
-	    oneResultPlayback.push_back(oneResult[j]->id());
-	  }
-	  ids[i-minBunch_] = oneResultPlayback;
-	}
-        LogDebug("readPileup") << "READ: " << oneResult.size();
-        std::copy(oneResult.begin(), oneResult.end(), std::back_inserter(eventVector));
-	n -= oneResult.size();
-      }
-      result.push_back(eventVector);
-    }
-  }
 
 } //namespace edm
